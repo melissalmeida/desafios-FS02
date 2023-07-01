8798fe1d81007ff1abaa914ef2d9edfe76cddbce
melissalmeida <dev.melissaag@gmail.com>
2023-06-30 21:10:08 -0300

    nova att

---
+++ script.js
@@ -0,0 +1,7 @@
+const usuario=document.getElementById("exampleInputEmail1");
+const senha=document.getElementById("exampleInputPassword1");
+if(username == "" || senha == "") {
+alert("Os campos usuário e senha são obrigatórios");
+} else {
+   // aqui vai o que o código fará se tiver usuário e senha - para testar, você pode colocar 'alert("Funcionou");' ou algo parecido.
+}
