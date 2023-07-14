27fe320ea577c4edcbd892ef29befa420d15784c
melissalmeida <dev.melissaag@gmail.com>
2023-07-14 20:57:25 -0300

    footer ajusted

--- script.js
+++
@@ -1,8 +0,0 @@
-function validacao_form (){
-    if(document.getElementById("exampleInputEmail1").value == ""){
-    alert('Por favor, preencha o campo nome');
-    document.getElementById("exampleInputEmail1").focus();
-    return false
-    }}
-
-validacao_form
